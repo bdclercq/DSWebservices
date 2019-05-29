5aa51f64573d35cd94a209f7c1201053c984685f
Beau De Clercq <bdclercq@iMac-van-Beau.local>
2019-05-29 19:48:37 +0200

    runs on mac

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-docker-compose -f docker-compose-dev.yml build
-docker-compose -f docker-compose-dev.yml up -d
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
